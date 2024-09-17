36f6db41cf4cdb66518823596415fc72a4136582
SW0301 <kartashov_01@list.ru>
2024-09-17 12:06:21 +0300

    Added calculation of vacation pay, taking into account holidays and weekends.

--- src/main/java/com/vacation_calculator/Service/VacationService.java
+++ src/main/java/com/vacation_calculator/Service/VacationService.java
@@ -3,6 +3,7 @@ package com.vacation_calculator.Service;
 import com.vacation_calculator.Model.VacationRequest;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public interface VacationService {
      BigDecimal calculateVacationPay(VacationRequest request);
